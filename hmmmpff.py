1000ea16628d984c38397f64a06064486618fb61
Denzel <chiagozieani145@gmail.com>
2020-01-28 05:21:50 +0100

    I really need to eat right now

--- hmmmpff.py
+++ hmmmpff.py
@@ -1,11 +1,34 @@
 class Employee:
-    raise_amt = 1.04
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last):
         self.first = first
         self.last = last
         self.mail = first + '.' + last + '@gmail.com'
-        self.pay = pay
 
+    @property
+    def email(self):
+        return '{}.{}@gmail.com'.format(self.first, self.last)
+
+    @property
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('AH THAT ONE IS DONE')
+        self.first = None
+        self.last = None
+
+
+r = Employee('gbera', 'ojkweu')
+r.fullname = 'utI gbain'
+del r.fullname
+print(r.mail)
+print(r.fullname)
+# print(r.)
